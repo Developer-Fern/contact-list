823260249222bd96527f687fe666c10618d475aa
Fern <FernandoFlores.private@gmail.com>
2023-09-25 03:09:20 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
+import Contactlist from './components/Contactlist'
 import './App.css'
+import SelectedContact from './components/SelectedContact'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedContactId, setSelectedContactId] = useState(null)
 
   return (
     <>
+      {selectedContactId ? (
+        <SelectedContact
+          selectedContactId={selectedContactId}
+          setSelectedContactId={setSelectedContactId}
+        />
+      ) : (
+        <Contactlist setSelectedContactId={setSelectedContactId} />
+      )}
     </>
   )
 }
